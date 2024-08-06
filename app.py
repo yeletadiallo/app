2f28df04a6f23d68585f479519204889bb25605d
yaya <yeleta2014yaya@gmail.com>
2024-08-06 14:03:45 +0000

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
-    'first column': [12, 2, 3, 4],
-    'second column': [15, 20, 30, 40]
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
 }))
